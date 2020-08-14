42b95e960dc4e653a0f58ddea176908800da90f6
Andre Pierre <andre.laurence94@gmail.com>
2020-08-14 00:29:55 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ def join_nested_strings(src)
     in_idx = 0 
     
     while in_idx < src[i].count do 
-      if src[i][in_idx].class == src[i][in_idx].to_s
+      if src[i][in_idx].class == String
       stringArr += src[i][in_idx].to_s
      end
     in_idx += 1 
